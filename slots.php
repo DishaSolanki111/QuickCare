43e7e38219ad7899261cbb287ebb3c68a31899f6
DishaSolanki111 <solankidisha236@gmail.com>
2025-12-18 13:57:04 +0530

    slot buttons

--- slots.php
+++ slots.php
@@ -1,30 +1,27 @@
 <?php
 include 'config.php';
 
-$doctor_id = $_GET['doctor_id'];
-$date = $_GET['date'];
+ $doctor_id = $_GET['doctor_id'];
+ $date = $_GET['date'];
 
-$q = mysqli_query($conn,"
+ $q = mysqli_query($conn,"
 SELECT SCHEDULE_ID, START_TIME, END_TIME
 FROM doctor_schedule_tbl
 WHERE DOCTOR_ID=$doctor_id
 ");
 
-$row = mysqli_fetch_assoc($q);
+ $row = mysqli_fetch_assoc($q);
 
-$start = strtotime($row['START_TIME']);
-$end   = strtotime($row['END_TIME']);
+ $start = strtotime($row['START_TIME']);
+ $end   = strtotime($row['END_TIME']);
 ?>
 <h3>Select Slot</h3>
-
-
 <?php
 while($start < $end){
     $slot = date("H:i",$start);
+    $redirectUrl = "login.php?doctor_id=" . $doctor_id . "&date=" . $date . "&time=" . $slot . "&schedule_id=" . $row['SCHEDULE_ID'];
     ?>
-    <a href="login.php?doctor_id=<?= $doctor_id ?>&date=<?= $date ?>&time=<?= $slot ?>&schedule_id=<?= $row['SCHEDULE_ID'] ?>">
-        <div class="slot"><?= $slot ?></div>
-    </a>
+    <button class="slot" onclick="window.location.href='<?= $redirectUrl ?>'"><?= $slot ?></button>
     <?php
     $start = strtotime("+1 hour",$start);
 }
